d813191928d2051e1707f329d71f54f1cc9d822e
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-10 17:06:11 -0300

    fix: consertando o cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import express, { Request, Response, NextFunction } from 'express';
 import { routes } from './routes/index.js';
 import { errorHandler } from './middlewares/my-middleware.js';
 
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
 app.use(routes);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 const PORT = process.env.PORT || 3000;
 
