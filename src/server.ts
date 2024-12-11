d00e5ff1f25db7e77382aac811142fb8c4e22df4
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-11 18:31:32 -0300

    fix: consertando api

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,12 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import { routes } from './routes/index.js';
-import { errorHandler } from './middlewares/my-middleware.js';
 
 const app = express();
 
+app.use(cors({origin: '*'}));
+
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
