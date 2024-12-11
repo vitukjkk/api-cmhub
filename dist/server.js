820b18f56928d6933961612e4164960f985478ae
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-11 19:16:26 -0300

    fix: consertando build

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes/index.js';
 const app = express();
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(routes);
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({ message: 'Hello World from Vitor!' });
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
